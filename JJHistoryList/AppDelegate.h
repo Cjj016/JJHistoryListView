ae223e87ce1f3f3ae9c5f85ab925d368e478d7ee
cjj016 <cjj016@163.com>
2019-01-23 17:46:47 +0800

    Initial Commit

---
+++ JJHistoryList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JJHistoryList
+//
+//  Created by Joey Chan on 2019/1/23.
+//  Copyright © 2019年 Joey Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JJHistoryList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JJHistoryList
+//
+//  Created by Joey Chan on 2019/1/23.
+//  Copyright © 2019年 Joey Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
